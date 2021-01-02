fd55e5b4d7b61edf3e90a8f69a643732b9aeb1d1
SebbyBlack <sebbyvonblack@gmail.com>
2021-01-01 19:44:03 -0600

    Created character list

---
+++ characterList.py
@@ -0,0 +1,8 @@
+from characters import Enemy
+from characters import NPC
+from roomList import Room 
+
+
+enemies = {
+    'skeleton': Enemy('Skeleton', "village", 20, ['Rusty Sword'],  ['Wooden Shield'])
+}
