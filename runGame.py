da5f174e7f83ceafa259866a16b932eb8aa50b0a
SebbyBlack <sebbyvonblack@gmail.com>
2021-01-01 19:51:17 -0600

    moved linking of rooms to its own file

--- runGame.py
+++ runGame.py
@@ -21,30 +21,11 @@ from player import Player
 from room import Room
 from roomList import room
 from items import Item
+import linkRooms
 
 
 
-# Linking Rooms
-room['village'].n_to = room['path']
-room['village'].w_to = room['blacksmith']    
-room['village'].e_to = room['apothecary']
-room['blacksmith'].s_to = room['village']
-room['apothecary'].s_to = room['village']
-room['path'].s_to = room['village']
-room['path'].n_to = room['lostForestEntrance']
-room['lostForestEntrance'].w_to = room['river']
-room['lostForestEntrance'].n_to = room['lostForestPath']
-room['lostForestEntrance'].s_to = room['path']
-room['lostForestPath'].w_to = room['clearing']
-room['lostForestPath'].n_to = room['caveEntrance']
-room['river'].s_to = room['lostForestEntrance']
-room['river'].e_to = room['clearing']
-room['clearing'].s_to = room['lostForestPath']
-room['clearing'].w_to = room['river']
-room['clearing'].n_to = room['cabin']
-room['caveEntrance'].s_to = room['lostForestPath']
-room['cabin'].s_to = room['clearing']
-room['cabin'].n_to = room['cabinInside']
+
 
 # Add Items to rooms
 room['village'].inventory = [
